9f059d12ef9c8d6609d3e2adc2647f040424958a
barust-a <alexandrebarusta@gmail.com>
2024-11-27 12:36:36 +0100

    add limit to fetch

--- data/sql/infra/shopifyBookImage.repository.ts
+++ data/sql/infra/shopifyBookImage.repository.ts
@@ -8,18 +8,20 @@ export async function deleteShopifyBookImageByIds(imageIds: string[]): Promise<v
   await sql(query, [imageIds])
 }
 
-export async function fetchDuplicatedShopifyBookImage(): Promise<ShopifyBookImageDb[]> {
+/** it select all group excluding last one to avoid deleting image that we dont want */
+export async function fetchDuplicatedShopifyBookImage(limit: number): Promise<ShopifyBookImageDb[]> {
   const query = `SELECT book_id, image_id, created_at
-    FROM shopifyBookImage
-    WHERE book_id IN (
-          SELECT book_id
-          FROM shopifyBookImage
-          GROUP BY book_id
-          HAVING COUNT(*) > 1
-    )
-    ORDER BY 
-      book_id,
-      created_at DESC;`
+  FROM shopifyBookImage
+  WHERE book_id IN (
+        SELECT book_id
+        FROM shopifyBookImage
+        GROUP BY book_id
+        HAVING COUNT(*) > 1
+  )
+  ORDER BY 
+    book_id,
+    created_at DESC
+  LIMIT $1`
 
-  return await sql(query)
+  return await sql(query, [limit])
 }
--- shopify/service/deleteDuplicateImages.ts
+++ shopify/service/deleteDuplicateImages.ts
@@ -4,18 +4,28 @@ import { deleteImageFromShopify } from "../infra/shopifyApi";
 import { groupBy, sortBy } from "./utils";
 
 
+/** everything is sorted by desc  */
 export async function deleteDuplicateImagesController(): Promise<void> {
-  const duplicateImages = await fetchDuplicatedShopifyBookImage()
-  const groupedByBookId = groupBy(duplicateImages, 'book_id')
+  const duplicateImages = await fetchDuplicatedShopifyBookImage(1000)
+  const filtered = removeIncompleteGroup(duplicateImages) // due to limit teh last group may be incomplete
+
+  const groupedByBookId = groupBy(filtered, 'book_id')
   for (const bookId of groupedByBookId) {
-    const booksToDelete = filterImageToDelete(groupedByBookId[bookId])
-    await callDeleteImageApiAndDeleteRowInDB(booksToDelete)
+    const imagesToDelete = extractImagesToDelete(groupedByBookId[bookId])
+    await callDeleteImageApiAndDeleteRowInDB(imagesToDelete)
   }
 
 }
 
-function filterImageToDelete(bookImages: ShopifyBookImageDb[]): ShopifyBookImageDb[] {
-  const sorted = sortBy(bookImages, 'desc')
+function removeIncompleteGroup(bookImages: ShopifyBookImageDb[]): ShopifyBookImageDb[] {
+  const sorted = sortBy(bookImages, 'desc', 'created_at')
+  const lastBookId = sorted[sorted.length - 1].book_id
+  const filtered = sorted.filter((b) => b.book_id !== lastBookId)
+  return filtered
+}
+
+function extractImagesToDelete(bookImages: ShopifyBookImageDb[]): ShopifyBookImageDb[] {
+  const sorted = sortBy(bookImages, 'desc', 'created_at')
   return sorted.shift() // remove not to delete
 }
 
